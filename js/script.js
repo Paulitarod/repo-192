8d1c427d37883d45c66fb1c58e4b1c40e4a700cd
Paula Rodriguez <mariapaularodriguezmontes@gmail.com>
2023-03-21 16:37:43 -0300

    subiendo ´primeros archivos

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+var auto = "Corolla"
+auto= "308"
+auto= "C4"
+console.log (auto)
+
+/*reasignè valor; redeclara es volver a poner var auto*/
+
+let nomnbre = "Paula"
+let apellido= "Rodrìguez"
+console.log ("Paula", "Rodrdriguez")
+
+var fruta = "naranja"
+var tipoDeLaVariableFruta = "fruta"
+console.log ("fruta")
